e14af4e6ee09f3830fc1bcab1d9a4954ef6717f0
Shri-0 <shrinath.narayanan@gmail.com>
2023-07-05 16:38:17 -0700

    "linkedlist error"

--- Basic Ones/linkedList.py
+++ Basic Ones/linkedList.py
@@ -1,14 +1,14 @@
 def cycle(head):
-	slow = head
-	fast = head
 
-	while fast is not None and fast.next is not None:
-		fast = fast.next.next
-		slow = slow.next
+    slow, fast = head, head
 
-		if slow == fast:
-			return True
-		return False
+    while fast is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+
+        if slow == fast:
+            return True
+    return False
 
 
 class Node:
